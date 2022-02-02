9116a40d68adef14ce0180b8c990d66e0390575f
R3tr8 <39610870+R3tr8@users.noreply.github.com>
2022-02-01 23:49:45 -0400

    creation d'un user et publication d'un post dans la db

---
+++ index.js
@@ -0,0 +1,39 @@
+const {PrismaClient} = require('@prisma/client')
+const prisma = new PrismaClient()
+
+async function main() {
+    await prisma.user.create({
+        data: {
+            pseudo: 'Peter Parker',
+            email: 'spidey@prisma.io',
+            posts: {
+                create: {title: 'First Post'},
+            },
+            profile: {
+                create: {bio: 'I am Spiderman'},
+            },
+        }
+    })
+
+    const allUsers = await prisma.user.findMany({
+        include: {
+            posts: true,
+            profile: true,
+        },
+    })
+    console.dir(allUsers, {depth: null})
+
+    const posts = await prisma.post.update({
+        where: {id: 1},
+        data: {published: true},
+    })
+    console.log(posts)
+}
+
+main()
+    .catch((e) => {
+        throw e
+    })
+    .finally(async () => {
+        await prisma.$disconnect()
+    })
